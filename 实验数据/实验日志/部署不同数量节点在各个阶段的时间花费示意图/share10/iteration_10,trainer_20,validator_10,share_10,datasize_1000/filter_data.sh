12a9a0b69ccfb463640e28e0831fd994388f04bd
wisecoach <827165648@qq.com>
2025-07-10 12:13:30 +0800

    上传申诉使用的实验数据

---
+++ 实验数据/实验日志/部署不同数量节点在各个阶段的时间花费示意图/share10/iteration_10,trainer_20,validator_10,share_10,datasize_1000/filter_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p filter_data
+cat all.log | grep "aggregate global" | awk '{print $1}' > filter_data/aggregate.txt
+cat all.log | grep -e "to aggregate" -e "global model begin" | awk '{print $1}' > filter_data/wait.txt
+logs=$(ls | grep 100)
+for log in $logs
+do
+	cat $log | grep "start iteration" | awk '{print $1}' > 'filter_data/'"$log"'_iteration.txt'
+	cat $log | grep -e "begin to wait " -e "receive all validate" | awk '{print $1}' > 'filter_data/'"$log"'_validate.txt'
+	cat $log | grep "trainer/model/train" | awk '{print $1}' > 'filter_data/'"$log"'_train.txt'
+done
